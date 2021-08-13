92e1909eb81b14d61ba96f37c757790c0747b6f6
chifly <chifly1999@163.com>
2021-08-13 13:11:13 +0800

    add login

--- src/main/java/com/example/chh/wx/ChhWxApiApplication.java
+++ src/main/java/com/example/chh/wx/ChhWxApiApplication.java
@@ -4,6 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.ServletComponentScan;
 
+
+
 @SpringBootApplication
 @ServletComponentScan
 public class ChhWxApiApplication {
